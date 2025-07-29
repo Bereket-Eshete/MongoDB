320dacbbac6903c6a144788b629b28a3b1f3979e
Bereket-Eshete <bereketeshete63@gmail.com>
2025-07-29 11:09:34 +0300

    add database connection to the server

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const bookRoutes = require("./routes/bookRoutes");
+const { connectToDb, getDb } = require("./db");
 const app = express();
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+let db;
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+    db = getDb();
+  }
 });
+
 app.use("/books", bookRoutes);
